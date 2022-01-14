6da14241d0b521832c2934c8edf642f219b03418
trzyq0712 <j.trzyq@gmail.com>
2022-01-14 20:42:34 +0100

    initial UI

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+//@ts-check
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var router = require('./routes/routes');
 
 var app = express();
 
@@ -19,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/play', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
