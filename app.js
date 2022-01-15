f53717a40cb7ab6a3b8cda8eb38edef5550a514b
trzyq0712 <j.trzyq@gmail.com>
2022-01-15 18:58:43 +0100

    splash screen

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/play', router);
+app.get('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/play', function(req, res, next) {
+router.get('/play', function (req, res, next) {
   res.render('game.ejs', { title: 'Express' });
 });
+
+router.get('/', function (req, res, next) {
+    res.render('splash.ejs');
+})
 
 module.exports = router;
